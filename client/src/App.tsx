fa252948ee745163687ccde48ae5481a99fbeb4f
Dennis Durairaj <dennis.durairaj@gmail.com>
2021-08-20 17:59:33 +0200

    Create basic nav with logo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./App.css";
 import RecipeList from "./features/recipes/RecipeList";
+import logo from "./assets/images/logo_transparent.png";
 
 function App() {
   return (
-    <div>
-      <RecipeList />
+    <div className="wrapper">
+      <nav>
+        <div className="logo">
+          <img src={logo} alt="Logo" />
+        </div>
+      </nav>
+      {/* <RecipeList /> */}
     </div>
   );
 }
